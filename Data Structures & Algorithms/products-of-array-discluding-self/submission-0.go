13df74a95f134d83a5963ba88898e7b836d635f6
Max Takuro <133218511+TakuroBreath@users.noreply.github.com>
2026-08-20 23:25:04 +0300

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.go
@@ -0,0 +1,20 @@
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	for i := range res {
+		res[i] = 1
+	}
+
+	prefix := 1
+	for i, num := range nums {
+		res[i] = prefix
+		prefix *= num
+	}
+
+	postfix := 1
+	for i := len(nums)-1; i >= 0; i-- {
+		res[i] *= postfix
+		postfix *= nums[i]
+	}
+
+	return res
+}
